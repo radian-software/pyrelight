de01d012d8523b4b2be433478f021ab4ed8228f9
Radon Rosborough <radon.neon@gmail.com>
2020-03-19 21:29:15 -0600

    Add server and client scripts

---
+++ pyrelight/__main__.py
@@ -0,0 +1,11 @@
+import os
+import pathlib
+import sys
+
+import pyrelight.server
+
+print("Starting PYRELIGHT server...", file=sys.stderr)
+
+os.chdir(pathlib.Path(__file__).parent.parent.resolve())
+
+pyrelight.server.listen("server", lambda msg: "echoing: " + msg)
