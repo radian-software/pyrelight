75720de1996abe4013c23436a09d876e6d3cc5fa
Radon Rosborough <radon.neon@gmail.com>
2020-03-20 19:27:48 -0600

    Pass through CLI to message-passing server

--- pyrelight/__main__.py
+++ pyrelight/__main__.py
@@ -1,11 +1,15 @@
 import os
 import pathlib
+import shlex
 import sys
 
+import pyrelight.cmdline
 import pyrelight.server
 
 print("Starting PYRELIGHT server.", file=sys.stderr)
 
 os.chdir(pathlib.Path(__file__).parent.parent.resolve())
 
-pyrelight.server.listen("server", lambda msg: "echoing: " + msg)
+pyrelight.server.listen(
+    "server", lambda msg: repr(pyrelight.cmdline.parser.parse_args(shlex.split(msg))),
+)
